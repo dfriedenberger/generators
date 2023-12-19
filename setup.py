a8476d9170ae9ba1428e35325c30caf0122951d7
Dirk Friedenberger <Dirk.Friedenberger@deutschebahn.com>
2023-12-19 15:38:46 +0100

    fit project structure

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from generators import __version__
 
 setup(name='generators',
       version=__version__,
-      description='Tool for Creating plantuml Files from rdf model.',
+      description='Tool for generating assets from rdf model.',
+      long_description=open('README.md', encoding="UTF-8").read(),
+      long_description_content_type='text/markdown',
       url='https://github.com/dfriedenberger/generators.git',
       author='Dirk Friedenberger',
       author_email='projekte@frittenburger.de',
       license='GPLv3',
       packages=['generators'],
       scripts=['bin/gen'],
-      install_requires=[],
+      install_requires=['chevron'],
       zip_safe=False)
